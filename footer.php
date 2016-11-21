8221bce690653ce419ffcf8ec538acd0ea876593
Vinh Nguyen <vxnguyen01@gmail.com>
2016-11-21 12:51:25 -0500

    First commit

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package tdandpartners
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'tdandpartners' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'tdandpartners' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'tdandpartners' ), 'tdandpartners', '<a href="https://github.com/vxnguyen" rel="designer">Vinh Nguyen</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
