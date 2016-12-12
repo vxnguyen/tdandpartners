fe2b83aedcc9b2acb764758d16e52eaf899441d8
Vinh Nguyen <vxnguyen01@gmail.com>
2016-12-12 14:51:27 -0500

    add final content and styles

--- front-page.php
+++ front-page.php
@@ -37,11 +37,11 @@ get_header(); ?>
 
 						<div class="carousel-inner" role="listbox">
 							<div class="item active">
-						      <a href="http://tdandpartners.com/projects/the-muse"><img src="<?php bloginfo('template_directory'); ?>/images/the-muse.jpg" alt="The Muse"></a>
+						      <a href="<?php echo get_page_link(86); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/the-muse.jpg" alt="The Muse"></a>
 						    </div>
 
 						    <div class="item">
-						      <a href="http://tdandpartners.com/projects/the-hilltop"><img src="<?php bloginfo('template_directory'); ?>/images/the-hilltop.jpg" alt="The HillTop"></a>
+						      <a href="<?php echo get_page_link(88); ?>"><img src="<?php bloginfo('template_directory'); ?>/images/the-hilltop.jpg" alt="The HillTop"></a>
 							</div>
 						</div>
 
@@ -58,12 +58,12 @@ get_header(); ?>
 						<div id="slider_captions">
                     		<div>
                         		<div id="caption-0" class="carousel-caption">
-                           			<a href=""><h2>The Muse</h2></a>
+                           			<h2>The Muse</h2>
                            			<p>On June 2, 2016, we broke ground on the largest mixed-use new construction project in Plainfield, New Jersey in over 40 years. The $30 million multi-phase development will consist of the construction of a new 44,000 square foot state-of-the-art facility for ABC Supply Company Inc., a major wholesale supplier of building products, followed by the construction of a 5-story multi-family building comprised of 90 residential units. </p>
                         		</div>
 
                         		<div id="caption-1" class="carousel-caption">
-                            		<a href=""><h2>The HillTop</h2></a>
+                            		<h2>The HillTop</h2>
                            			<p>Construction is underway on “The HillTop,” a $200 million dollar redevelopment project that will transform the former Irvington General Hospital site into vibrant community consisting of 700 residential units, a family friendly restaurant, and a pre-school.</p>
                        			</div>
                    		 	</div>
@@ -71,7 +71,19 @@ get_header(); ?>
               		</div>
 				</div>
 
+				<div class="firm-distinction-sidebar">
+					<div class="firm">
+						<h2>Firm</h2>
+						<p><?php the_field('firm-content'); ?></p>
+						<a href="<?php echo get_page_link(14); ?>"><div class="read-more-arrow"></div>Community Impact</a>
+					</div>
 
+					<div class="distinction">
+						<h2>Distinction</h2>
+						<p><?php the_field('distinction-content'); ?></p>
+						<a href="<?php echo get_page_link(12); ?>"><div class="read-more-arrow"></div>Our Proven Model</a>
+					</div>
+				</div>
 			</section>
 
 		</main><!-- #main -->
