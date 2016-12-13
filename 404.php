64031a3da3c2e577e9c757fcdf6620cf75467dca
Vinh Nguyen <vxnguyen01@gmail.com>
2016-12-13 16:39:01 -0500

    Add content to 404 page and update styles

--- 404.php
+++ 404.php
@@ -10,12 +10,16 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main site-404" role="main">
 
 			<section class="inner error-404 not-found">
 				<header class="page-header">
 					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'tdandpartners' ); ?></h1>
 				</header><!-- .page-header -->
+
+				<div class="page-content">
+					<p><?php esc_html_e( 'It looks like nothing was found at this location. Please try using one of the navigation links above', 'tdandpartners' ); ?></p>
+				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
 		</main><!-- #main -->
